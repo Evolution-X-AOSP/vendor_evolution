f34bff1a1b94467dfd251433cebe5dcb1cb2e6e0
ElDainosor <eldainosor@gmail.com>
2018-11-11 14:16:10 +0530

    QSTile: Add Squaremedo from BootLeggers [2/2]
    Change-Id: Iec1301279db484e69ced7952f16873adc14e478e
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -183,7 +183,8 @@ PRODUCT_PACKAGES += \
     QStileDottedCircle \
     QStileNinja \
     QStilePokesign \
-    QStileWavey
+    QStileWavey \
+    QStileSquaremedo
 
 # Navbar
 PRODUCT_PACKAGES += \
