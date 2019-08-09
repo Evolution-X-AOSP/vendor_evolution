2b489f72b3f53433056bae875b95758b0848fed1
Akito Mizukito <peak@echln.net>
2019-08-09 22:10:41 +0900

    vendor: tools: Make the generator suit the current OTA system needs
    Signed-off-by: Akito Mizukito <peak@echln.net>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -8,6 +8,6 @@ then
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
-    echo "{\n   \"file_name\": \"$file_name\",\n   \"file_size\": $file_size,\n   \"datetime\": $datetime,\n   \"id\": \"$id\",\n   \"md5\": \"$md5\"\n}" > $file_path.json
+    echo "{\n   \"filename\": \"$file_name\",\n   \"size\": $file_size,\n   \"datetime\": $datetime,\n   \"id\": \"$id\",\n   \"filehash\": \"$md5\"\n}" > $file_path.json
   fi
 fi
