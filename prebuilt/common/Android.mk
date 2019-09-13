ba32a5352bd1160037a265d2f1da5527247941dd
Bruno Martins <bgcngm@gmail.com>
2019-09-13 16:11:04 +0100

    common: Copy APN list to $(TARGET_COPY_OUT_PRODUCT)/etc
    This handles GSI product
    
    Change-Id: I5754ff9c020e094c5ca5bd08e0572584009558ee

--- prebuilt/common/Android.mk
+++ prebuilt/common/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_USES_AOSP_APNS_CONF),)
 ################################
-# Copies the APN list file into system/etc for the product as apns-conf.xml.
+# Copies the APN list file into $(TARGET_COPY_OUT_PRODUCT)/etc for the product as apns-conf.xml.
 # In the case where $(CUSTOM_APNS_FILE) is defined, the content of $(CUSTOM_APNS_FILE)
 # is added or replaced to the $(DEFAULT_APNS_FILE).
 include $(CLEAR_VARS)
@@ -26,6 +26,8 @@ FINAL_APNS_FILE := $(DEFAULT_APNS_FILE)
 endif
 
 LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
+
+LOCAL_PRODUCT_MODULE := true
 
 include $(BUILD_PREBUILT)
 endif
