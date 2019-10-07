2da68a1abd1bb258b01bc52a1533ecb25c04c2fc
あきと ミズキト <peak@echln.net>
2019-10-08 02:15:57 +0900

    Fixed JSON file_size parsing

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -4,7 +4,7 @@ then
   file_path=$1
   file_name=$(basename "$file_path")
   if [ -f $file_path ]; then
-    file_size=$(stat --printf="%s" "$file_path")
+    file_size=$(stat -c%s $file_path)
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
