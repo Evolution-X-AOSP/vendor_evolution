4280edeff7ee59ee82379b30812162e22511ff31
Bruno Martins <bgcngm@gmail.com>
2019-11-21 15:50:28 +0000

    Revert "vendor: add BUILD_RRO_SYSTEM_PACKAGE target"
    AOSP pushes RROs to system by default now,
    therefore our custom target is not needed anymore.
    
    This reverts commit 546c9a1ec50392d77851302742a7f338a8426cad.
    
    Change-Id: I46753a3b60a89f0198058fbc6d07d3db8a6699b5

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,5 +1,3 @@
-BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/evolution/build/core/system_rro.mk
-
 # We modify several neverallows, so let the build proceed
 ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
