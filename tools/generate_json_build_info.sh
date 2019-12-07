13a94e03d9ad220b6c344870c09d66acaccc6970
Stallix <team.evolutionx.rom@gmail.com>
2019-12-07 07:30:54 +0000

    vendor: tools: Make the generator follow the OTA format

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -8,6 +8,6 @@ then
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
-    echo "{\n   \"filename\": \"$file_name\",\n   \"size\": $file_size,\n   \"datetime\": $datetime,\n   \"id\": \"$id\",\n   \"filehash\": \"$md5\"\n}" > $file_path.json
+    echo "{\n   \"filename\": \"$file_name\",\n   \"datetime\": $datetime,\n   \"size\": $file_size,\n   \"filehash\": \"$md5\",\n   \"id\": \"$id\"\n}" > $file_path.json
   fi
 fi
