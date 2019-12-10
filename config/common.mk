696adca93baaadf36879d942bdd3e899941bb0aa
Stallix <team.evolutionx.rom@gmail.com>
2019-12-10 18:25:21 +0000

    config: Disable priv-app permissions whitelist

--- config/common.mk
+++ config/common.mk
@@ -87,7 +87,7 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 # Power whitelist
 PRODUCT_COPY_FILES += \
