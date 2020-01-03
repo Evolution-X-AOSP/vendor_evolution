LineageOS/android_vendor_lineage

vendor: make dopush recognize files on more partitions
Change-Id: Ied1a243730df1d683fd237f8ea7bd9b1742db105
Signed-off-by: Roman Birg <romanbirg@gmail.com>

build/envsetup.sh
@@ -887,9 +887,9 @@ EOF
 
     stop_n_start=false
     for TARGET in $(echo $LOC | tr " " "\n" | sed "s#.*${RELOUT}##" | sort | uniq); do
-        # Make sure file is in $OUT/system or $OUT/data
+        # Make sure file is in $OUT/system, $OUT/data, $OUT/odm, $OUT/oem, $OUT/product, $OUT/product_services or $OUT/vendor
         case $TARGET in
-            /system/*|/data/*)
+            /system/*|/data/*|/oem/*|/odm/*|/product/*|/product_services/*|/vendor/*)
                 # Get out file from target (i.e. /system/bin/adb)
                 FILE=$OUT$TARGET
             ;;

Evolution-X-AOSP/vendor_evolution

build/envsetup.sh
@@ -810,9 +810,9 @@ EOF
 
     stop_n_start=false
     for TARGET in $(echo $LOC | tr " " "\n" | sed "s#.*${RELOUT}##" | sort | uniq); do
-        # Make sure file is in $OUT/system or $OUT/data
+        # Make sure file is in $OUT/system, $OUT/data, $OUT/odm, $OUT/oem, $OUT/product, $OUT/product_services or $OUT/vendor
         case $TARGET in
-            /system/*|/data/*)
+            /system/*|/data/*|/odm/*|/oem/*|/product/*|/product_services/*|/vendor/*)
                 # Get out file from target (i.e. /system/bin/adb)
                 FILE=$OUT$TARGET
             ;;