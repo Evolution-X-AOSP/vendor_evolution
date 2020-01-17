73813ed25d730105172f7d1c55d5e69ff3a79263
Cruz <du.alexcruz@gmail.com>
2020-01-17 14:57:38 +0100

    ColorBucket: Add Solarized Dark theme
    arrow-edits:
     - Adapt to ColorBucket overlay controller.
    
    Change-Id: I7281bd24f8eff831797180987d25f1c6c36a1eea
    Co-Authored-By: ganeshi4u <ganeshvarmai4u@gmail.com>
    Signed-off-by: Akito Mizukito <akito@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -150,7 +150,9 @@ PRODUCT_PACKAGES += \
     SystemStyleOverlay \
     SystemUIStyleOverlay \
     SystemNightOverlay \
-    SystemUINightOverlay
+    SystemUINightOverlay \
+    SystemSolarizedDarkOverlay \
+    SystemUISolarizedDarkOverlay
 
 # Themed bootanimation
 TARGET_MISC_BLOCK_OFFSET ?= 0
