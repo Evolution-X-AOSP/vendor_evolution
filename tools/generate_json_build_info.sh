1da63c5174ac98c1f63d07f070ff3ecf88c2dbf3
Joey Huab <joey@evolution-x.org>
2020-03-08 20:33:16 +0000

    vendor: tools: Sort the json to suit the current OTA system

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -8,6 +8,6 @@ then
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
-    echo "{\n   \"filename\": \"$file_name\",\n   \"datetime\": $datetime,\n   \"size\": $file_size,\n   \"filehash\": \"$md5\",\n   \"id\": \"$id\"\n}" > $file_path.json
+    echo "{\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size\n}" > $file_path.json
   fi
 fi
