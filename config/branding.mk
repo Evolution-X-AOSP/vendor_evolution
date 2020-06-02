b0258f8a4300686c1b36843ea4e31042c217c476
Joey Huab <joey@evolution-x.org>
2020-06-02 08:38:04 +0000

    June patch is a headache

--- config/branding.mk
+++ config/branding.mk
@@ -1,6 +1,6 @@
 # Versioning System
-EVO_BASE_VERSION := 4.3
-EVO_CODENAME := Undisputed
+EVO_BASE_VERSION := 4.4
+EVO_CODENAME := Underground
 EVO_DONATE_URL ?= Unknown
 EVO_MAINTAINER ?= Unknown
 EVO_SUPPORT_URL ?= https://t.me/EvolutionX
