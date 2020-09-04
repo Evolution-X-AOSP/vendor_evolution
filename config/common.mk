7e6003e0b4d72f115e744766bbb35c77e7b23cea
Joey Huab <joey@evolution-x.org>
2020-09-04 11:09:09 +0000

    vendor: Build AOSP ThemePicker
    * Since Goog has been a bitch to their Wallpapers app

--- config/common.mk
+++ config/common.mk
@@ -171,6 +171,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     StitchImage
 
+# ThemePicker
+PRODUCT_PACKAGES += \
+    ThemePicker
+
 # Cutout control overlay
 ifneq ($(filter true, $(TARGET_PROVIDES_OWN_NO_CUTOUT_OVERLAY)),)
 PRODUCT_PACKAGES += NoCutoutOverlay
