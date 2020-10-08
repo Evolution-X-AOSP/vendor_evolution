c7dfc54aaed02d436ed2d04013e81736e48ecfd7
Sweeto143 <sipunkumar85@gmail.com>
2020-10-08 13:06:14 +0000

    vendor: Use PRODUCT_GENERIC_PROPERTIES
    - Fixes ExternalError: couldn't find ro.superior.display.version in build.prop
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I8c984ba344a850774f17052ac334877c98e63833
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/ota.mk
+++ config/ota.mk
@@ -2,7 +2,7 @@ ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 
 CUSTOM_OTA_VERSION_CODE := ten
 
-CUSTOM_PROPERTIES += \
+PRODUCT_GENERIC_PROPERTIES += \
     org.evolution.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
     sys.ota.disable_uncrypt=1
 
