1c4b4f8769e897ddd0a1e457d31a19cea519fecb
Omkar Chandorkar <forumomkar@gmail.com>
2020-10-13 21:32:35 +0530

    evolution: apex: fix up tabbing
    tabs vs spaces huh
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/apex.mk
+++ config/apex.mk
@@ -25,18 +25,18 @@ PRODUCT_PACKAGES += \
 # Google Apexes
 PRODUCT_PACKAGES += \
     com.google.android.adbd \
-	com.google.android.cellbroadcast \
-	com.google.android.conscrypt \
-	com.google.android.extservices \
-	com.google.android.ipsec \
-	com.google.android.media \
-	com.google.android.media.swcodec \
-	com.google.android.mediaprovider \
-	com.google.android.neuralnetworks \
-	com.google.android.os.statsd \
-	com.google.android.permission \
-	com.google.android.resolv \
-	com.google.android.sdkext \
-	com.google.android.telephony \
-	com.google.android.tzdata2 \
-	com.google.android.wifi
+    com.google.android.cellbroadcast \
+    com.google.android.conscrypt \
+    com.google.android.extservices \
+    com.google.android.ipsec \
+    com.google.android.media \
+    com.google.android.media.swcodec \
+    com.google.android.mediaprovider \
+    com.google.android.neuralnetworks \
+    com.google.android.os.statsd \
+    com.google.android.permission \
+    com.google.android.resolv \
+    com.google.android.sdkext \
+    com.google.android.telephony \
+    com.google.android.tzdata2 \
+    com.google.android.wifi
