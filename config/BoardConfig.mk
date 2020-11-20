a3586b49025332647bf0993280bf95797d1a68ea
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:50:43 -0300

    vendor: Allow forcing custom build fingerprint
    Change-Id: I783a19829cb9d655f8abaeafa14f1f3d6a12ed20
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -1,3 +1,6 @@
+# Safetynet
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+
 include vendor/evolution/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
