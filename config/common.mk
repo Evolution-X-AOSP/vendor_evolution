13e98301e0fdbf1fac525c29ca620ea58056930d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-12-01 12:03:26 -0300

    vendor: Remove useless flag
    Change-Id: Ibfefb730854053b7a88ca7852b92ec462547808f
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -36,12 +36,6 @@ PRODUCT_COPY_FILES += \
     vendor/evolution/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/evolution/prebuilt/common/bin/50-base.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-base.sh
 
-# OTA
-ifneq ($(TARGET_BUILD_VARIANT),user)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.ota.allow_downgrade=true
-endif
-
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
     vendor/evolution/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
