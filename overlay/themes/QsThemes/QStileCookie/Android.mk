390b4bad5b1b108f2686085fd0487ad0810a8a7b
NurKeinNeid <mralexman3000@gmail.com>
2021-01-30 16:11:35 +0100

    QSTiles: Build in product and as RRO package
    Change-Id: Ib44586f519ef0c5202ab841b8a1a8890e6ec74c8
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- overlay/themes/QsThemes/QStileCookie/Android.mk
+++ overlay/themes/QsThemes/QStileCookie/Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-CAL_MODULE_TAGS := optional
+
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_PACKAGE_NAME := QStileCookie
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := false
-
-include $(BUILD_PACKAGE)
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_RRO_PACKAGE)
