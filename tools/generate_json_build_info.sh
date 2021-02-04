c9e86439419b1be64330467cfcf826e9d5f18c04
Joey Huab <joey@evolution-x.org>
2021-02-04 17:31:15 +0900

    vendor: tools: Use a different datetime format
    * Much smarter to use the line from https://github.com/Evolution-X/build/blob/elle/tools/buildinfo.sh#L21 instead.
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -3,11 +3,10 @@ if [ "$1" ]
 then
   file_path=$1
   file_name=$(basename "$file_path")
-  DEVICE=$(echo $TARGET_PRODUCT | cut -d "_" -f2)
   if [ -f $file_path ]; then
     file_size=$(stat -c%s $file_path)
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-    datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+    datetime=date +%s
     id=$(sha256sum $file_path | awk '{ print $1 }');
     echo "{\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size\n}" > $file_path.json
   fi
