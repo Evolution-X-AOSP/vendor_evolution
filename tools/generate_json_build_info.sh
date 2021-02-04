74ea7cbec7fdc849ee2aa63b8df6623dca3b26ff
Justin Crawford <Justin@stacksmash.net>
2021-02-04 17:34:03 +0530

    vendor: tools: Fix json generation for codenames with underscores
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -3,11 +3,12 @@ if [ "$1" ]
 then
   file_path=$1
   file_name=$(basename "$file_path")
+  DEVICE=$(echo $TARGET_PRODUCT | sed 's/evolution_//g')
   if [ -f $file_path ]; then
     file_size=$(stat -c%s $file_path)
     md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-    datetime=date +%s
+    datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
     id=$(sha256sum $file_path | awk '{ print $1 }');
-    echo "{\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size\n}" > $file_path.json
+    echo -e "{\n   \"datetime\": $datetime,\n   \"filehash\": \"$md5\",\n   \"filename\": \"$file_name\",\n   \"id\": \"$id\",\n   \"size\": $file_size\n}" > $file_path.json
   fi
 fi
