b5c41647f8be46ae01c68914428e5dd3446f4a21
Joey Huab <joey@evolution-x.org>
2021-04-08 14:21:17 +0900

    ColorBucket: Raven > Pitch Black

--- overlay/themes/ColorBucket/PitchBlack/SystemPitchBlack/Android.mk
+++ overlay/themes/ColorBucket/PitchBlack/SystemPitchBlack/Android.mk
@@ -17,7 +17,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := SystemRavenBlack
+LOCAL_RRO_THEME := SystemPitchBlack
 
 LOCAL_PRODUCT_MODULE := true
 
@@ -25,7 +25,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-LOCAL_PACKAGE_NAME := SystemRavenBlackOverlay
+LOCAL_PACKAGE_NAME := SystemPitchBlackOverlay
 LOCAL_SDK_VERSION := current
 
 include $(BUILD_RRO_PACKAGE)
