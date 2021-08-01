c83fea142f6e6f4f6c1c40bbdfdd3f7ca4f79a86
Alexander Koskovich <zvnexus@outlook.com>
2021-08-01 08:54:51 -0700

    vendor: Add framework compatibility matrix for Lineage HALs.
     * Cover all possible Lineage HALs here instead of covering
       them in the device specific manifests.
    
    Change-Id: Id7f7d6eb1f14190a061537cebfabd339af9b1620

--- config/common.mk
+++ config/common.mk
@@ -207,6 +207,8 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/evolution/overlay
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/evolution/overlay/common
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/evolution/config/device_framework_matrix.xml
+
 # Plugins
 #include packages/apps/Plugins/plugins.mk
 
