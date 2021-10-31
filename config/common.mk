4831062d161244faff58315a8cd0fe0b460a9a41
Dyneteve <dyneteve@hentaios.com>
2021-10-31 20:36:55 +0100

    evolution: config: Import SetupWizard/IME props from SD1A.210817.015.A4
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -36,7 +36,6 @@ PRODUCT_BRAND ?= EvolutionX
 
 # Gboard configuration
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.ime.bs_theme=true \
     ro.com.google.ime.theme_id=5 \
     ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
 
@@ -46,9 +45,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.setupwizard.esim_cid_ignore=00000001 \
     ro.setupwizard.rotation_locked=true \
     setupwizard.feature.baseline_setupwizard_enabled=true \
-    setupwizard.feature.device_default_dark_mode=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.portal_notification=true \
     setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
     setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
     setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
     setupwizard.theme=glif_v3_light
 
