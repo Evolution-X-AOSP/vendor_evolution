2eb19362ed6861ce46ac5495631368038dbedd49
Joey Huab <joey@evolution-x.org>
2022-05-10 03:32:47 +0900

    config: Copy bootanimation to system/media for pixel devices

--- config/common.mk
+++ config/common.mk
@@ -224,7 +224,11 @@ $(call inherit-product, vendor/evolution/config/apps.mk)
 $(call inherit-product, vendor/evolution/config/audio.mk)
 
 # Inherit from bootanimation config
+ifneq ($(filter violet walleye taimen blueline crosshatch bonito sargo flame coral sunfish oriole raven, $(TARGET_DEVICE)),)
+$(call inherit-product, vendor/evolution/config/bootanimation_pixels.mk)
+else
 $(call inherit-product, vendor/evolution/config/bootanimation.mk)
+endif
 
 # Inherit from our branding
 $(call inherit-product, vendor/evolution/config/branding.mk)
