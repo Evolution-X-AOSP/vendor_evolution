5efa8df6ec4eb497bd50a05fea4dc9700c5f873a
Dyneteve <dyneteve@hentaios.com>
2022-06-12 15:34:17 +0200

    evolution: CarrierSettings: Update from SQ3A.220605.009.A1
    Change-Id: I5488a3d5cb3ed2a7b8f79aed3ec2135bfe421a7e

--- config/telephony.mk
+++ config/telephony.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
     liberty_pr.pb \
     linemo_jp.pb \
     luckymobile_ca.pb \
+    m1_sg.pb \
     metropcs_us.pb \
     movistar_es.pb \
     o2_de.pb \
@@ -177,6 +178,7 @@ PRODUCT_PACKAGES += \
     virgin_gb.pb \
     virgin_us.pb \
     visible_us.pb \
+    visiblev_us.pb \
     vodafone_au.pb \
     vodafone_cz.pb \
     vodafone_de.pb \
