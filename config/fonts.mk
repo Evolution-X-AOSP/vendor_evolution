55b6a5ccb48f107f207d7fd6bb35399ebbee4ba9
minaripenguin <alexfinhart@gmail.com>
2022-07-13 08:10:25 +0800

    themes: fonts: Add ProductSans VH
    * google sans with vietnamese support
    * forwarded from a user named "Twelvetican", credits goes to the respective owner.
    
    Signed-off-by: minaripenguin <alexfinhart@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -68,6 +68,7 @@ PRODUCT_PACKAGES += \
     FontNunitoSourceOverlay \
     FontOdudaSourceOverlay \
     FontOneUISansOverlay \
+    FontProductSansVHOverlay \
     FontQuandoSourceOverlay \
     FontRedressedSourceOverlay \
     FontReemKufiSourceOverlay \
