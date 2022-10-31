ec2a562ef5a01b116ef8dc7b3d330a3450186184
AnierinB <anierin@evolution-x.org>
2022-10-31 05:48:21 +0000

    Revert "vendorsetup: Use recursive find & replace to remove the ndk_platform backend"
    * No longer needed
    
    This reverts commit fe9d6f92b36ce3d60fb177f92c5edbc2a5c15349.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,3 @@ do
         lunch_others_targets+=("evolution_$device-$var")
     done
 done
-
-(cd vendor/qcom && find . -type f -exec sed -i 's/ndk_platform/ndk/g' {} + )
-(cd hardware && find . -type f -exec sed -i 's/ndk_platform/ndk/g' {} + )
