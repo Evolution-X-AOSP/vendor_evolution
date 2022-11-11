a7567bf78eda0a6a6dabb4044fd12a429078ea1e
Joey Huab <joey@evolution-x.org>
2022-11-11 14:05:50 +0000

    Revert "[TEMP] vendor: Allow neverallows for now"
    This reverts commit fa04e757c8a56ef739454b1ba7e5a2f9c6f632f5.

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,7 +1,2 @@
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Rules for QCOM targets
 include $(TOPDIR)vendor/evolution/build/core/qcom_target.mk
