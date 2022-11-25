cfda0511dcc35ea0d8101fdc67092505de69bbcc
someone5678 <nemui3353@gmail.com>
2022-11-25 14:29:55 +0900

    vendor: config: common: Build BtHelper

--- config/common.mk
+++ config/common.mk
@@ -35,6 +35,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.carriersetup.vzw_consent_page=true
 
+# BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
+
 # Use gestures by default
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
