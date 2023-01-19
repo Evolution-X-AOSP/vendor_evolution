e3f06bbc09a9779764e9dbf300d5145557b9b39e
El Dainosor <eldainosor@gmail.com>
2023-01-19 09:04:02 -0300

    clockfont: Replace Sedgwick Ave with Permanent Marker
    Segdwick is a cool font but the padding is horrible. Let's replace it with something something in the "same style".

--- config/themes.mk
+++ config/themes.mk
@@ -100,7 +100,6 @@ PRODUCT_PACKAGES += \
     ClockFontBikoOverlay \
     ClockFontGinoraSansOverlay \
     ClockFontRivieraOverlay \
-    ClockFontSedgwickAveOverlay \
     ClockFontUnionOverlay \
     ClockFontAccuratistOverlay \
     ClockFontAclonicaOverlay \
@@ -150,4 +149,5 @@ PRODUCT_PACKAGES += \
     ClockFontRubikGlitchOverlay \
     ClockFontTourneyMediumOverlay \
     ClockFontVG5000Overlay \
-    ClockFontOdibeeSansOverlay
+    ClockFontOdibeeSansOverlay \
+    ClockFontPermanentMarkerOverlay
