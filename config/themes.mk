65b67507dbdfedaa098289faaec4a343f94d880c
F5LAYD <73395630+F5LAYD@users.noreply.github.com>
2023-02-21 23:24:09 +0530

    themes: Add Dora navbar style
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/themes.mk
+++ config/themes.mk
@@ -33,6 +33,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NavbarAndroidOverlay \
     NavbarAsusOverlay \
+    NavbarDoraOverlay \
     NavbarMotoOverlay \
     NavbarNexusOverlay \
     NavbarOldOverlay \
