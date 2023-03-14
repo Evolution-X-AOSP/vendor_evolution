92c97d9d042c0d4bea77a0f5a680d894d6986dce
Tim Zimmermann <tim@linux4.de>
2023-03-14 07:05:52 +0100

    config: Inherit window_extensions.mk
    * Instead of explicitly building androidx.window.extensions
    
    Change-Id: I107ef8ef49eab77d91aa06fe3ade55eeaae83c71

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -1,8 +1,6 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
 # Inherit full common Evolution X stuff
 $(call inherit-product, vendor/evolution/config/common_full.mk)
-
-# Required packages
-PRODUCT_PACKAGES += \
-    androidx.window.extensions
 
 $(call inherit-product, vendor/evolution/config/telephony.mk)
--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,6 +1,4 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
 # Inherit full common Evolution X stuff
 $(call inherit-product, vendor/evolution/config/common_full.mk)
-
-# Required packages
-PRODUCT_PACKAGES += \
-    androidx.window.extensions
