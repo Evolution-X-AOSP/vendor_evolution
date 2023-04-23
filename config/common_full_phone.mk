5781e299c2ce66246029c9ebdd5419ed797dbe91
Joey Huab <joey@evolution-x.org>
2023-04-23 16:01:55 +0900

    Revert "Revert "config: Build NavigationBarMode2ButtonOverlay""
    This reverts commit a0f589ac3db731984d629f1d0a4b78dbfafcec9e.

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,10 @@
 # Inherit full common Evolution X stuff
 $(call inherit-product, vendor/evolution/config/common_full.mk)
 
+# Required packages
+PRODUCT_PACKAGES += \
+    NavigationBarMode2ButtonOverlay
+
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
