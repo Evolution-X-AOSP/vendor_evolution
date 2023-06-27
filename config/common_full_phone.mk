fa5afd9d64ec044b1d9adf37b2e98f3ca25a19c2
Joey Huab <joey@evolution-x.org>
2023-06-27 23:43:38 +0900

    Revert "config: Build desktop mode packages"
    This reverts commit ff0f1562ce61eef0d7c9cfbf8e753aad4ab8937b.

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -5,15 +5,6 @@ $(call inherit-product, vendor/evolution/config/common_full.mk)
 PRODUCT_PACKAGES += \
     NavigationBarMode2ButtonOverlay
 
-# DesktopMode
-PRODUCT_PACKAGES += \
-    DesktopMode
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
-
-$(call inherit-product, packages/services/VncFlinger/product.mk)
-
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
