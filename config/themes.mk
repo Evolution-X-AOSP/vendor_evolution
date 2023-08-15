69afb86b92966179987d13b63c9a10a173820076
rezaadi0105 <rezaadipangestu5@gmail.com>
2023-08-15 06:31:29 +0000

    Themes: Data icon style [3/3]
    Overlays: Add data icons from iconify
    Credit: https://github.com/Mahmud0808/Iconify
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>
    Signed-off-by: helliscloser <mrumais@gmail.com>

--- config/themes.mk
+++ config/themes.mk
@@ -98,6 +98,22 @@ PRODUCT_PACKAGES += \
     XperiaWiFiOverlay \
     ZigZagWiFiOverlay
 
+# Data icons
+PRODUCT_PACKAGES += \
+    AuroraDataOverlay \
+    FaintUIDataOverlay \
+    GradiconDataOverlay \
+    LornDataOverlay \
+    PlumpyDataOverlay
+
+# Icon Data
+PRODUCT_PACKAGES += \
+    IconDataFive \
+    IconDataFour \
+    IconDataOne \
+    IconDataThree \
+    IconDataTwo
+
 # Themes
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay \
