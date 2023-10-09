5f859826cbd38fb12869fb1e7909d097ea944d9a
Michael Bestas <mkbestas@lineageos.org>
2023-10-09 16:10:05 +0300

    RIP Android Beam
    Change-Id: Ia977097d40b7f0538f2b15d231e69f2fa22adc4c

--- config/common.mk
+++ config/common.mk
@@ -119,10 +119,6 @@ PRODUCT_COPY_FILES += \
     vendor/evolution/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml \
     vendor/evolution/config/permissions/org.lineageos.livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.livedisplay.xml
 
-# Enable Android Beam on all targets
-PRODUCT_COPY_FILES += \
-    vendor/evolution/config/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.nfc.beam.xml
-
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml
