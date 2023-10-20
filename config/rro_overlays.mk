18a189e206cca9315ce10d63053af0390eaac757
aswin7469 <aswinas@pixysos.com>
2023-10-20 22:35:56 +0530

    vendor: Update pixel overlays
    * updated from https://github.com/hentaiOS/platform_vendor_hentai/tree/Ursamoon/rro_overlays
    * cleanup a bit for our needs
    * keep everything as rro
    
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -40,9 +40,10 @@ PRODUCT_PACKAGES += \
     PixelConfigOverlay2019Midyear \
     PixelConfigOverlayCommon \
     PixelDocumentsUIGoogleOverlay \
+    PixelFwOverlay \
     PixelLauncherCustomOverlay \
     PixelSetupWizardOverlay \
-    SettingsGoogleOverlayPixel2022 \
+    SettingsGoogleOverlay \
     SettingsProviderOverlay \
     SystemUIGoogleOverlay \
     TeleServiceOverlay \
