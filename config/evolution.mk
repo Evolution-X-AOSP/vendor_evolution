4009f78e7613a62481b68e49ec63a14d968fd1c1
John Galt <johngaltfirstrun@gmail.com>
2024-01-02 11:36:34 -0500

    evolution: Add preinstalled package list to stop auto cloning
    Only extra adds needed, gms now ships it's own lists for this.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- config/evolution.mk
+++ config/evolution.mk
@@ -6,3 +6,7 @@ ifeq ($(EVO_BUILD_TYPE),Official)
 PRODUCT_PACKAGES += \
     Updater
 endif
+
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml
