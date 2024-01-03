bad5fa4c79ed4e2a99e307a95bcbd0bf5ce05c61
Aaron Kling <webgeek1234@gmail.com>
2024-01-02 21:13:18 -0600

    kernel: Default to no gcc on kernel 6.x
    Current logic defaults to no gcc if kernel major version is greater than
    or equal to 5 and kernel minor version is greater than or equal to 10.
    Meaning kernel version 6.1 does not qualify because 1 is less than 10.
    So additionally check if kernel major version is greater than or equal
    to 6 and ignore minor version for that case.
    
    Change-Id: Id72cb9e100c6fed014d696f4a3a88f6cafcd3932

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2023 The LineageOS Project
+# Copyright (C) 2018-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -78,7 +78,9 @@ TARGET_KERNEL_CLANG_AOSP ?= true
 
 # 5.10+ can fully compile without GCC by default
 ifneq ($(KERNEL_VERSION),)
-    ifeq ($(shell expr $(KERNEL_VERSION) \>= 5), 1)
+    ifeq ($(shell expr $(KERNEL_VERSION) \>= 6), 1)
+        TARGET_KERNEL_NO_GCC ?= true
+    else ifeq ($(shell expr $(KERNEL_VERSION) \>= 5), 1)
         ifeq ($(shell expr $(KERNEL_PATCHLEVEL) \>= 10), 1)
             TARGET_KERNEL_NO_GCC ?= true
         endif
