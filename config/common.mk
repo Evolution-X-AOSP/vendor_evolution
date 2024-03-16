hentaiOS/platform_vendor_hentai

animations: Import pixel off-charging animation
Test: Verify charging animation.

Change-Id: I70dacfedbd6ab6234b9020c929cfe8d4c22fe661
Signed-off-by: Dyneteve <dyneteve@hentaios.com>

config/common.mk
@@ -70,6 +70,9 @@ PRODUCT_PACKAGE_OVERLAYS += \
 # Inherit from lewdboratory config
 $(call inherit-product-if-exists, vendor/lewdboratory/packages.mk)
 
+# Inherit from animations config
+$(call inherit-product, vendor/hentai/config/animations.mk)
+
 # Inherit from apex config
 $(call inherit-product, vendor/hentai/config/apex.mk)
 

Evolution-X-AOSP/vendor_evolution

config/common.mk
@@ -264,6 +264,9 @@ else
 $(call inherit-product-if-exists, vendor/partner_modules/build/mainline_modules_flatten_apex.mk)
 endif
 
+# Inherit from animations config
+$(call inherit-product, vendor/evolution/config/animations.mk)
+
 # Inherit from audio config
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
 $(call inherit-product, vendor/evolution/config/audio.mk)