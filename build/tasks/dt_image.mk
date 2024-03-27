TheParasiteProject/vendor_aosp

vendor: build: tasks: Move from lz4 to lz4c
* LineageOS no longer ships lz4
* Use lz4c instead

Change-Id: Ie4efb59ce0da7f76c4816c42179e8b11b097dbe9

build/tasks/dt_image.mk
@@ -42,7 +42,7 @@ endif
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
 	$(build-dtimage-target)
 ifeq ($(strip $(BOARD_KERNEL_LZ4C_DT)),true)
-	prebuilts/tools-lineage/${HOST_OS}-x86/bin/lz4 -9 < $@ > $(LZ4_DT_IMAGE)
+	prebuilts/tools-lineage/${HOST_OS}-x86/bin/lz4c -c1 -y $@ $(LZ4_DT_IMAGE)
 	$(hide) $(ACP) $(LZ4_DT_IMAGE) $@
 endif
 	@echo "Made DT image: $@"

Evolution-X-AOSP/vendor_evolution

build/tasks/dt_image.mk
@@ -42,7 +42,7 @@ endif
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(INSTALLED_KERNEL_TARGET)
 	$(build-dtimage-target)
 ifeq ($(strip $(BOARD_KERNEL_LZ4C_DT)),true)
-	prebuilts/tools-evolution/${HOST_OS}-x86/bin/lz4 -9 < $@ > $(LZ4_DT_IMAGE)
+	prebuilts/tools-evolution/${HOST_OS}-x86/bin/lz4c -c1 -y $@ $(LZ4_DT_IMAGE)
 	$(hide) $(ACP) $(LZ4_DT_IMAGE) $@
 endif
 	@echo "Made DT image: $@"