214e1446a907a2dfb5d742c3f185856b59f2eebe
LuK1337 <priv.luk@gmail.com>
2024-04-21 10:01:48 +0200

    config: Enable settings large screen optimization for tablets
    This was set to true by default prior to U QPR2.
    
    Test: Open settings and notice that dual pane layout is back
    Change-Id: Ide44ecbe7e30d6bf26d9c9358e46c71b35c66250

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,5 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
+# Settings
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.settings.large_screen_opt.enabled=true
+
 # Inherit full common Evolution X stuff
 $(call inherit-product, vendor/evolution/config/common_full.mk)
 $(call inherit-product, vendor/gms/gms_full_tablet_wifionly.mk)
